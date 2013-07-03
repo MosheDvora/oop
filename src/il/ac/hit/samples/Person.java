037b490eda002d60274be69a468cdba271c38eaf
Moshe Dvora <moshe.dvora@gmail.com>
2013-07-03 12:02:48 +0300

    commite 5 push 1

--- src/il/ac/hit/samples/Person.java
+++ src/il/ac/hit/samples/Person.java
@@ -4,7 +4,7 @@ public class Person {
 	private String name;
 	private double height;
 	private int id;
-	//test commite 5 push
+	//test commite 5 push1
 		
 	public Person(String nameVal, double heightVal, int idVal) {
 		this.name = nameVal;
